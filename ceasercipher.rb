0bbf43cd452b5c78419b6498dbbcda6f08765ebe
riomljr <22552727+riomljr@users.noreply.github.com>
2022-12-31 16:33:17 -0600

    fixing variables and add comments

--- ceasercipher.rb
+++ ceasercipher.rb
@@ -2,21 +2,21 @@ def ceaser_cipher(string, amount)
   alpha_min_values = *('a'..'z')
   alpha_cap_values = *('A'..'Z')
   alpha = alpha_cap_values + alpha_min_values
- 
+  convert = []
   capital=[]
   encrypted =[]
   i = 0
- 
+ #converting to number
   string.each_char do |char|
     if alpha.include?(char)
-      convert += ((alpha.find_index(char)))
-      capital.push(i)if char == char.upcase
+      convert.push((alpha.find_index(char)))
+      capital.push(i) if char == char.upcase #get cap letter indexes
     else
-      convert.push(char)
+      convert.push(char) #ignore non-num values and add them
     end
     i += 1
   end
-
+#converting num val to new char
   convert.each do |number|
     if number.is_a? Numeric
       num = number + amount 
@@ -26,14 +26,15 @@ def ceaser_cipher(string, amount)
       encrypted.push(number)
     end
   end
-
+#adjusting for upper and lower case in orignal string
   encrypted.each {|en| en.downcase!}
   capital.each {|value| encrypted[value].upcase!}
  
    print encrypted.join("")
 end
 
-
  ceaser_cipher("Hi How Are You?", 10)
+
+ ceaser_cipher("how are?", 5)
 
 
