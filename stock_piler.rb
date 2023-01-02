6dc183706c68f956cc47d377eb6329ad55e0a759
riomljr <22552727+riomljr@users.noreply.github.com>
2023-01-02 16:54:06 -0600

    cleaning up function

--- stock_piler.rb
+++ stock_piler.rb
@@ -1,10 +1,6 @@
 days_prices = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(prices)
-  len = prices.length
-  min = prices.min(len/2)
-  days = Hash.new(0)
-  indexes = []
   profit=[]
  
   prices.each_with_index do |value, i|
@@ -18,8 +14,6 @@ def stock_picker(prices)
       print "buy day #{i} and sell day #{x} " if (val - value) == profit.max
     end
   end
-
-
 end
 
 stock_picker(days_prices)
