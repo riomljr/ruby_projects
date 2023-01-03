96d7402469e362e140e5dfabcaa02dc6f94998bb
riomljr <22552727+riomljr@users.noreply.github.com>
2023-01-03 00:03:37 -0600

    removing sort fucntion

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,19 @@
 def bubble_sort(numbers)
-  until numbers.sort == numbers do
-    for index in 0...(numbers.length) -1
+  len = (numbers.length)
+  max_loops = len * len  
+
+  max_loops.times do
+    for index in 0...len - 1
       if numbers[index] > numbers[index + 1]
         move = numbers[index]
         numbers[index] = numbers[index + 1]
         numbers[index + 1] = move
-      end
+      end 
     end
   end
   numbers
 end
 
 bubble = [4,3,78,2,0,2]
-
-print bubble_sort(bubble)
+ 
+end
