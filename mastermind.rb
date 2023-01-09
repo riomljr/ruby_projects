9531a239e730cfa357b58c3c7d87ba6a9e63be30
riomljr <22552727+riomljr@users.noreply.github.com>
2023-01-09 11:15:52 -0600

    add user input method

--- mastermind.rb
+++ mastermind.rb
@@ -1,11 +1,31 @@
-module Colors
-  colors = [orange, red, blue, green, yellow, brown]
+
+class Player
+  attr_reader :colors
+  @options = ["orange", "red", "blue", "green", "yellow", "brown"]
+  def initialize(name)
+    @name = name
+    @colors =[]
+  end
+
+  def get_colors
+    print "(1)orange,(2)red, (3)blue, (4)green, (5)yellow, (6)brown \n Pick four Colors! \n"
+    for i in 1...5
+      puts " Choose Color number #{i}"
+      @colors.push(gets.chomp)
+    end
+  end
+
 end
 
-class player
+class Board
 
 end
 
-class board
+class Game
 
 end
+
+player1 = Player.new("Jefrrey")
+player1.get_colors
+
+print player1.colors
